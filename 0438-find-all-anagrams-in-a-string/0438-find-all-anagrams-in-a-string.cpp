e934b8462cfee7e670c20b21ef6b4c5f1ef6b05b
Devisingh Rajput <devisinghrajput.cg@gmail.com>
2026-07-21 14:28:05 +0530

    Time: 5 ms (56.98%), Space: 11.6 MB (63.49%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
+        int a=s.size();
+        int b=p.size();
+        int low=0;
+        int high=b-1;
         vector<int> ans;
+        vector<int> sCount(26,0),pCount(26,0);
+        if(a<b) return ans;
 
-        int n = s.size();
-        int k = p.size();
-
-        if (n < k) return ans;
-
-        vector<int> pFreq(26, 0);
-        vector<int> window(26, 0);
-
-        for (char c : p)
-            pFreq[c - 'a']++;
-
-        int left = 0;
-
-        for (int right = 0; right < n; right++) {
-
-            window[s[right] - 'a']++;
-
-            if (right - left + 1 > k) {
-                window[s[left] - 'a']--;
-                left++;
-            }
-
-            if (right - left + 1 == k && window == pFreq) {
-                ans.push_back(left);
-            }
+        for(auto c:p)
+        {
+            pCount[c-'a']++;
         }
 
+        for(int i=0; i<b; i++)
+        {
+            sCount[s[i]-'a']++;
+        }
+        if(sCount==pCount)
+        {
+            ans.push_back(0);
+        }
+while(high<a-1)
+{
+    sCount[s[low]-'a']--;
+    sCount[s[high+1]-'a']++;
+    if(sCount==pCount)
+        {
+            ans.push_back(low+1);
+        }
+
+        low++;
+        high++;
+}
         return ans;
     }
 };
