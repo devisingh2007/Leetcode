6f8eea50d5face99a8178e19f9ad81e38cacee76
Devisingh Rajput <devisinghrajput.cg@gmail.com>
2026-08-16 20:59:33 +0530

    Time: 447 ms (77.74%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE offsetVal INT;
+  SET offsetVal = N - 1;
+
+  RETURN (
+      SELECT DISTINCT Salary
+      FROM Employee
+      ORDER BY Salary DESC
+      LIMIT 1 OFFSET offsetVal
+  );
+END;
