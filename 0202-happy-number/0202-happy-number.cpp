46fe3190535c66f5e6ddb60796df544863bc5650
Devisingh Rajput <devisinghrajput.cg@gmail.com>
2026-09-07 13:54:22 +0530

    Time: 0 ms (100%), Space: 7.9 MB (67.18%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+       while(n!=1&&n!=4) 
+       {
+        int ans=0;
+        while(n>0)
+        
+        {
+            int digit=n%10;
+            ans=ans+digit*digit;
+            n=n/10;
+            
+        }
+        n=ans;
+        }
+        return n==1;
+    }
+};
