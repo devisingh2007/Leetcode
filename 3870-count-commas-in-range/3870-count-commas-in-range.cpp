e0529fa35f74334fb52a3c462b2ec5c16daa7b29
Devisingh Rajput <devisinghrajput.cg@gmail.com>
2026-09-08 11:32:07 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.47%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int count =0;
+        
+            if(n>=1000)
+            {
+                return n-999;
+            }
+        return 0;
+
+    }
+};
